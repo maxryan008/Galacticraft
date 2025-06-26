f42a4b306eefce849aa631cdfe0b4ab24ea56a10
maxryan008 <maxryan008@gmail.com>
2025-06-26 20:36:02 +1000

    Fluid tanks working
    -- WARNING --
    
    This commit has a major memory leak
    
    most likely within the LevelRendererMixin class

---
+++ src/main/java/dev/galacticraft/mod/mixin/client/RenderSectionAccessor.java
@@ -0,0 +1,20 @@
+package dev.galacticraft.mod.mixin.client;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.renderer.chunk.SectionRenderDispatcher;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+import java.util.Map;
+
+@Environment(EnvType.CLIENT)
+@Mixin(SectionRenderDispatcher.RenderSection.class)
+public interface RenderSectionAccessor {
+    @Accessor("buffers")
+    <K, V> Map<K,V> getBufferMap();
+
+    @Invoker("setCompiled")
+    void invokeSetCompiled(SectionRenderDispatcher.CompiledSection compiledSection);
+}
